4f3309f103e61761c854396151af37f04b736c0e
dlimla <danielhlim1990@gmail.com>
2019-04-16 20:34:27 -0700

    adds testing and snapshot for display

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,62 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {render, fireEvent, cleanup } from 'react-testing-library';
+import "jest-dom/extend-expect";
+afterEach(cleanup);
+
+import Controls from './Controls'
+
+describe('<Controls />', () => {
+    it('renders <Controls /> without crashing', () => {
+        render(<Controls />);
+    })
+
+    it('should create create a snapshot of <Controls/>', () => {
+        const tree = renderer.create(<Controls />);
+        expect(tree.toJSON()).toMatchSnapshot();
+    })
+
+    it('should have the locking button starting as "lock gate" as starting value', () => {
+        const { getByText } = render(<Controls />);
+        getByText(/lock gate/i);
+    })
+
+    it('should have a open button starting as close gate as starting value', () => {
+        const { getByText } = render(<Controls />);
+        getByText(/close gate/i);
+    })
+
+    it('should have lock gate disabled if gate is open as default value', () => {
+        const { getByText } = render(<Controls />);
+        const lockButton = getByText(/lock gate/i)
+        expect(lockButton).toBeDisabled();
+    })
+
+    it('should have the close gate enabled', () => {
+        const { getByText } = render(<Controls/>);
+        const closeButton = getByText(/close gate/i);
+        expect(closeButton).toBeEnabled()
+    })
+
+    it('should call toggleLocked on clicked', () => {
+        const toggleLockedMocking = jest.fn();
+        const { getByText } = render(
+            <Controls toggleLocked={toggleLockedMocking} closed={true}
+        />)
+
+        const lockButton = getByText(/lock gate/i);
+        fireEvent.click(lockButton);
+        expect(toggleLockedMocking).toBeCalledTimes(1);
+    })
+
+    it('should call the toggleClosed on click', () => {
+        const toggleCloseMock = jest.fn();
+        const { getByText } = render(<Controls toggleClosed = {toggleCloseMock}/>);
+        const closeButton = getByText(/close gate/i);
+        
+        fireEvent.click(closeButton);
+
+        expect(toggleCloseMock).toBeCalledTimes(1);
+    })
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,24 @@
-// Test away
+// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from 'react-testing-library';
+import "jest-dom/extend-expect";
+
+import Dashboard from './Dashboard'
+
+describe('<Dashboard />', () => {
+    //render test
+    it('renders dashboard without crashing', () => {
+        render(<Dashboard/>)
+    })
+
+    //snapshot dash test
+    it('should create a match snapshot of dashboard', () => {
+        const tree = renderer.create(<Dashboard />);
+
+        expect(tree.toJSON()).toMatchSnapshot();
+    })
+
+    
+
+})
