1fa2c295af3055967ab37f5beda47c286d702160
dlimla <danielhlim1990@gmail.com>
2019-04-16 20:55:38 -0700

    adds the tests and snapshot for dashboard MVP complete

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -19,6 +19,53 @@ describe('<Dashboard />', () => {
         expect(tree.toJSON()).toMatchSnapshot();
     })
 
+    it('changes the display from open to close after clicking close button', () => {
+        const {getByText} = render(<Dashboard/>);
+        const closeButton = getByText(/close gate/i)
+        const closedGate = getByText(/open/i)
+
+        fireEvent.click(closeButton);
+
+        expect(closedGate.textContent).toBe('Closed')
+    })
+
+    it('enables lock button after gate is closed', () => {
+        const { getByText } = render(<Dashboard/>);
+        const closeButton = getByText(/close gate/i);
+        const lockButton = getByText(/lock gate/i);
+        
+        fireEvent.click(closeButton);
+        
+        expect(lockButton).toBeEnabled();
+    })
+
+    it('disables the open button after gate is locked', () => {
+        const { getByText } = render(<Dashboard />);
+
+        const closeButton = getByText(/close gate/i);
+        const lockButton = getByText(/lock gate/i);
+
+        fireEvent.click(closeButton);
+        fireEvent.click(lockButton);
+
+        const openButton = getByText(/open gate/i);
+        expect(openButton).toBeDisabled();
+    })
     
+    it('enables to open the gate after it is unlocked', () => {
+        const { getByText } = render(<Dashboard/>);
+
+        const closeButton = getByText(/close gate/i);
+        const lockButton = getByText("Lock Gate");
+
+        fireEvent.click(closeButton);
+        fireEvent.click(lockButton);
+
+        const openButton = getByText(/open gate/i);
+        const unlock = getByText("Unlock Gate");
+        fireEvent.click(unlock);
+
+        expect(openButton).toBeEnabled();
+    })
 
 })
